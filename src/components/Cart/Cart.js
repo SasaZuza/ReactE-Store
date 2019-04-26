54ac1252f9eef6e92faed684314c6645fe328081
‘SasaZuza’ <sasazuzazule@gmail.com>
2019-04-26 18:58:25 +0200

    PayPal button added

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,45 +1,41 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 // importing 'Title' component from 'components' folder
-import Title from '../Title'
+import Title from '../Title';
 
 // Importing those components
-import CartColumns from './CartColumns'
-import EmptyCart from './EmptyCart'
+import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
 
 // Importing 'ProductConsumer' frrom 'context.js' file
-import { ProductConsumer } from '../../context'
+import { ProductConsumer } from '../../context';
 
 // Importing those components
-import CartList from './CartList'
-import CartTotals from './CartTotals'
-
-
+import CartList from './CartList';
+import CartTotals from './CartTotals';
 
 export default class Cart extends Component {
-    render() {
-        return (
-            <section>
-                <ProductConsumer>
-                    {value => {
-                        const { cart } = value;
-                        if (cart.length > 0) {
-                            return (
-                                <React.Fragment>
-                                    <Title name="my" title="cart" />
-                                    <CartColumns />
-                                    {/* Here we access values from 'context.js' file */}
-                                    <CartList value={value} />
-                                    <CartTotals value={value} />
-                                </React.Fragment>
-                            );
-                        } else {
-                            return (
-                                <EmptyCart />
-                            );
-                        }
-                    }}
-                </ProductConsumer>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section>
+        <ProductConsumer>
+          {value => {
+            const { cart } = value;
+            if (cart.length > 0) {
+              return (
+                <React.Fragment>
+                  <Title name='my' title='cart' />
+                  <CartColumns />
+                  {/* Here we access values from 'context.js' file */}
+                  <CartList value={value} />
+                  <CartTotals value={value} history={this.props.history} />
+                </React.Fragment>
+              );
+            } else {
+              return <EmptyCart />;
+            }
+          }}
+        </ProductConsumer>
+      </section>
+    );
+  }
 }
--- src/components/Cart/CartTotals.js
+++ src/components/Cart/CartTotals.js
@@ -1,48 +1,54 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PayPalButton from './PayPalButton';
 
-export default function CartTotals({ value }) {
+export default function CartTotals({ value, history }) {
+  // Here we did destructuring to access values from 'context.js'
+  const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
 
-    // Here we did destructuring to access values from 'context.js'
-    const { cartSubTotal, cartTax, cartTotal, clearCart } = value;
+  return (
+    <React.Fragment>
+      <div className='container'>
+        <div className='row'>
+          {/* Column elements */}
+          <div className='col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right'>
+            {/* Button - After clearing cart we want to go to homepage */}
+            <Link to='/'>
+              <button
+                className='btn btn-outline-danger text-uppercase mb-3 px-5'
+                type='button'
+                onClick={() => clearCart()}
+              >
+                Clear cart
+              </button>
+            </Link>
 
-    return (
-        <React.Fragment>
-            <div className="container">
-                <div className="row">
-                    {/* Column elements */}
-                    <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+            {/* Cart Subtotal value */}
+            <h5>
+              <span className='text-title'>Subtotal: </span>
+              <strong>$ {cartSubTotal}</strong>
+            </h5>
 
-                        {/* Button - After clearing cart we want to go to homepage */}
-                        <Link to="/">
-                            <button className="btn btn-outline-danger text-uppercase mb-3 px-5"
-                                type="button"
-                                onClick={() => clearCart()}>
-                                Clear cart
-                            </button>
-                        </Link>
+            {/* Cart Tax value */}
+            <h5>
+              <span className='text-title'>Tax: </span>
+              <strong>$ {cartTax}</strong>
+            </h5>
 
-                        {/* Cart Subtotal value */}
-                        <h5>
-                            <span className="text-title">Subtotal: </span>
-                            <strong>$ {cartSubTotal}</strong>
-                        </h5>
-
-                        {/* Cart Tax value */}
-                        <h5>
-                            <span className="text-title">Tax: </span>
-                            <strong>$ {cartTax}</strong>
-                        </h5>
-
-                        {/* Cart Subtotal value */}
-                        <h5>
-                            <span className="text-title">Total: </span>
-                            <strong>$ {cartTotal}</strong>
-                        </h5>
-
-                    </div>
-                </div>
-            </div>
-        </React.Fragment>
-    );
+            {/* Cart Subtotal value */}
+            <h5>
+              <span className='text-title'>Total: </span>
+              <strong>$ {cartTotal}</strong>
+            </h5>
+            {/* Inserting PayPalButton component with parameters */}
+            <PayPalButton
+              total={cartTotal}
+              clearCart={clearCart}
+              history={history}
+            />
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
 }
